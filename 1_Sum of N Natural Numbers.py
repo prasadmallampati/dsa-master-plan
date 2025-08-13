2f799321f959db0728d427a22534761d64b94ae6
prasadmallampati <[prasadmallampati16@gmail.com]>
2025-08-13 21:09:13 +0530

    # intial

---
+++ 1_Sum of N Natural Numbers.py
@@ -0,0 +1,26 @@
+#  input are two types static and dynamic ::---
+
+# output will use print, f string
+
+# loops are two type for(finite loop ),while(infinite)
+
+# sum of n natural numbers ::
+
+'''
+Algorithm ::---
+
+step 1 read n value which is an integer 
+step 2 initialize  sum = 0
+step 3 use loop from 1 to n , add each number to sum
+step 4 output result  
+
+'''
+
+
+# code 
+
+n = int(input("Enter n value:="))
+sum = 0 
+for i in range(1,n+1):
+    sum += i
+print("sum=",sum)
